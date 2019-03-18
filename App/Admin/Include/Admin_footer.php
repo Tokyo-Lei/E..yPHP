c4e749786d3b95a90026cc18970da7500ba24995
Naoki Ono <krosash@gmail.com>
2019-03-18 21:50:14 +0800

    更新Common路径

--- App/Admin/Include/Admin_footer.php
+++ App/Admin/Include/Admin_footer.php
@@ -3,10 +3,10 @@
 
 <!-- JavaScript files-->
 <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
-    <script src="<?php echo $PUBLIC_URL ?>popper.js/umd/popper.min.js"> </script>
-    <script src="<?php echo $PUBLIC_URL ?>bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?php echo $PUBLIC_URL ?>jquery.cookie/jquery.cookie.js"> </script>
-    <script src="<?php echo $PUBLIC_URL ?>jquery-validation/jquery.validate.min.js"></script>
+    <script src="<?php echo $PUBLIC_URL ?>Common/popper.js/umd/popper.min.js"> </script>
+    <script src="<?php echo $PUBLIC_URL ?>Common/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo $PUBLIC_URL ?>Common/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="<?php echo $PUBLIC_URL ?>Common/jquery-validation/jquery.validate.min.js"></script>
     <!-- Main File-->
     <script src="<?php echo $PUBLIC_URL ?>Admin/js/front.js"></script>
 
