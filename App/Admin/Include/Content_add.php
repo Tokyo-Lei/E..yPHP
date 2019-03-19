1b0321391062e44271a125b4586e2982695261c7
Tokyo-Lei <krosash@gmail.com>
2019-03-19 11:05:31 +0800

    自定义编辑器

--- App/Admin/Include/Content_add.php
+++ App/Admin/Include/Content_add.php
@@ -224,5 +224,4 @@ include 'nav.php';
 
     });
 
-    // ["undo","redo","|","bold","del","italic","quote","ucwords","uppercase","lowercase","|","h1","h2","h3","h4","h5","h6","|","list-ul","list-ol","hr","|","link","reference-link","image","code","preformatted-text","code-block","table","datetime","emoji","html-entities","pagebreak","|","goto-line","watch","preview","fullscreen","clear","search","|","help","info"],simple:["undo","redo","|","bold","del","italic","quote","uppercase","lowercase","|","h1","h2","h3","h4","h5","h6","|","list-ul","list-ol","hr","|","watch","preview","fullscreen","|","help","info"]
 </script>
